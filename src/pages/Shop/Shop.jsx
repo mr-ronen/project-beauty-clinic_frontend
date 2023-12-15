8d68d9f96f632dc30e4e01a03e67146625bc94ec
mr-ronen <112888529+mr-ronen@users.noreply.github.com>
2023-12-15 05:37:32 +0200

    fixed errors

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect } from 'react';
-import Header from '../../components/Header/Header'
-import { useSelector, useDispatch } from 'react-redux';
-import { setProducts } from '../../redux/slices/productSlice';
+import React, { useEffect } from "react";
+import Header from "../../components/Header/Header";
+import { useSelector, useDispatch } from "react-redux";
+import { setProducts } from "../../redux/slices/productSlice";
 // Assume you have a service to fetch products
-import productService from '../../services/productService';
+import productService from "../../services/productService";
 
-
-const Home = () => {
+const Shop = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.product.products);
 
@@ -16,18 +15,17 @@ const Home = () => {
     });
   }, [dispatch]);
 
-
   return (
     <>
-    <Header />
-    <h2>shop</h2>
-    <div>
+      <Header />
+      <h2>shop</h2>
+      <div>
         {products.map((product) => (
           <div key={product.id}>{product.name}</div>
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Shop;
